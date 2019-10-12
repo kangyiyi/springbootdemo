d536a792ca2b6ab6fc852179124157d0411c2f11
kangyiyi <40420741+kangyiyi@users.noreply.github.com>
2019-10-12 17:50:45 +0800

    20191012项目上传到Git

---
+++ springbootdemo-test/src/main/java/com/example/springbootdemotest/interfaces/LambdaNonReturnSingleParameter.java
@@ -0,0 +1,15 @@
+package com.example.springbootdemotest.interfaces;
+
+
+/**
+ * @author KangPz
+ * @Title: LambdaNonReturnSingleParameter
+ * @ProjectName springbootdemo
+ * @Description: TODO
+ * @company ccxcredit
+ * @date 2019/5/24-15:30
+ */
+@FunctionalInterface
+public interface LambdaNonReturnSingleParameter {
+    void test(int a);
+}
